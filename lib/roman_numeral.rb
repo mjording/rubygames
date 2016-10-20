8e9da273fe6c5f7d8a12f9e18dd5b47223a8f8f4
Matthew Jording <mjording@gmail.com>
2016-10-19 20:31:20 -0400

    almost there

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,10 +1,55 @@
 class RomanNumeral
+  attr_accessor :roman_string, :decimal_val
+
+  SPECIALS = {
+    'CM': 900,
+    'CD': 400,
+    'XC': 90,
+    'XL': 40,
+    'IX': 9,
+    'IV': 4,
+  }
+
+  HASHROMAN = {
+    'CM': 900,
+    'CD': 400,
+    'XC': 90,
+    'XL': 40,
+    'IX': 9,
+    'IV': 4,
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'V': 5,
+    'I': 1,
+  }
+
   def initialize( numeral_string )
     @roman_string = numeral_string
+    @decimal_val = RomanNumeral.decimal_value(numeral_string)
   end
 
-  def +( numeral_string )
-    "MMX"
+  def self.romanize(decimal)
+    reverse_hashroman = HASHROMAN.invert
+
+  end
+
+  def self.decimal_value(numerical_string)
+    working_roman = numerical_string
+    total = 0
+      HASHROMAN.each{|num,val|
+        count = working_roman.scan(/#{num}/).count
+        total += count * val
+        working_roman = working_roman.gsub(/#{num}/,'')
+      }
+    total
+  end
+
+  def +( other_roman )
+    dec_num = other_roman.decimal_val + decimal_val
+    RomanNumeral.romanize(dec_num)
   end
 
   def -( numeral_string )
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -6,8 +6,8 @@ describe RomanNumeral do
 
   it 'should compute simple arithmatic' do
     @reports << Benchmark.measure do
-      expect( RomanNumeral.new( 'MCMXCVI' ) + RomanNumeral.new( 'XIV' ) ).to be('MMX')
-      expect( RomanNumeral.new( 'MMX' ) + RomanNumeral.new( 'XIV' ) ).to be('MCMXCVI')
+      expect( RomanNumeral.new( 'MCMXCVI' ) + RomanNumeral.new( 'XIV' ) ).to eq('MMX')
+      expect( RomanNumeral.new( 'MMX' ) + RomanNumeral.new( 'XIV' ) ).to eq('MCMXCVI')
     end
   end
 
